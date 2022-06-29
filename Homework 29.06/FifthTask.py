dc17fa8b908235d4fead2bc5ce6bfa658c8dfbbf
Nathalie <nathalie@gmail.com>
2022-06-29 20:52:45 +0200

    Updated.

--- Homework 29.06/FifthTask.py
+++ Homework 29.06/FifthTask.py
@@ -5,17 +5,12 @@ numbers_x = [10, 20, 30, 40, 10]
 numbers_y = [75, 65, 35, 75, 30]
 
 
-def same_or_not_numbers_x():
-    if numbers_x[0] == numbers_x[-1]:
-        return True
-    else:
-        return False
+def same_or_not_numbers(numbers):
+    return numbers[0] == numbers[-1]
 
-def same_or_not_numbers_y():
-    if numbers_y[0] == numbers_y[-1]:
-        return True
-    else:
-        return False
 
-print("The first and last element of numbers_x is:", numbers_x[0], numbers_x[-1])
-print("The first and last element of numbers_y is:", numbers_y[0], numbers_y[-1])
+# print("The first and last element of numbers_x is:", numbers_x[0], numbers_x[-1])
+# print("The first and last element of numbers_y is:", numbers_y[0], numbers_y[-1])
+
+print(same_or_not_numbers(numbers_x))
+print(same_or_not_numbers(numbers_y))
